dec2ea68cb4afabaf46dec18f50f2c523b4c9ed1
Henry Chukwu <henry.chukwu@andela.com>
2018-07-05 15:02:49 +0100

    Add projects page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+});
+
 app.get('/badRequest', (req, res) => {
   res.send({
     errorMesaage: 'This reuest went bad'
